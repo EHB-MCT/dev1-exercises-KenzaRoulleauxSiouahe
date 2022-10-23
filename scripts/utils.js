a6e4316f27ee0ddfbadad29ef6a24d2a0182e0d3
Kenza Roulleaux-Siouahe <kenza.roulleaux.siouahe@ehb.be>
2022-10-23 20:37:47 +0200

    oefening gemaakt

---
+++ scripts/utils.js
@@ -0,0 +1,14 @@
+"use strict"
+
+import context from "./context.js";
+
+export function drawLine(x1,y1,x2,y2) {
+    context.beginPath();
+    context.moveTo(x1,y1);
+    context.lineTo(x2,y2);
+    context.stroke();
+
+export function strokeCircle(x,y,radius)
+    context.beginPath();
+    context.moveTo()
+}
